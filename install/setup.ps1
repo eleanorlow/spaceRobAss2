4a9a1da5830ba669071f6f674cc153c5b93de60e
eleanor low <eleanor.low@outlook.com>
2025-09-10 15:24:34 +1000

    from ubantu REAL

--- install/setup.ps1
+++ install/setup.ps1
@@ -23,9 +23,6 @@ function _colcon_prefix_chain_powershell_source_script {
 
 # source chained prefixes
 _colcon_prefix_chain_powershell_source_script "/opt/ros/humble\local_setup.ps1"
-_colcon_prefix_chain_powershell_source_script "/home/eleanorlow/spcrob/pythonTutorial/colcon_ws/install\local_setup.ps1"
-_colcon_prefix_chain_powershell_source_script "/home/eleanorlow/robo1/install\local_setup.ps1"
-_colcon_prefix_chain_powershell_source_script "/home/eleanorlow/spcrob/ros_ws/install\local_setup.ps1"
 
 # source this prefix
 $env:COLCON_CURRENT_PREFIX=(Split-Path $PSCommandPath -Parent)
